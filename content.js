278bb3b3b68997e78ff7615fbc3a089420b60146
Walter <wakyj07@gmail.com>
2021-06-13 11:52:20 +0300

    Update code to match end of the video

--- content.js
+++ content.js
@@ -1,53 +1,69 @@
-let sideNav, iconDetailSidebarOpen = false, 
-    downloadButton, copyButton;
+// Replace download SVG button
+// Fetch the svg based on href of the download button
+// Copy fetched svg into clipboard
 
-window.onload = function(){
-    sideNav = document.querySelector(".drawer--icon-details");
-    var observer = new MutationObserver(function(mutations) {
-        const classesChanged = mutations.find(({attributeName}) => attributeName === "class");
-        if(classesChanged)
-            handleClassesChanged();
-    });
-    
-    if(sideNav)
-        observer.observe(sideNav, {attributes: true});
+let downloadButton, copyButton, toast;
+
+window.addEventListener("load", function(){
+    const iconDetailsSidebar = this.document.querySelector(".drawer--icon-details");
+    // Listen for changes in class on icon detail sidebar
+    setupSidebarListener(iconDetailsSidebar);
+    setupToast();
+});
+
+function setupToast(){
+    toast = document.createElement("div");
+    toast.id = "copyMaterialIconToast";
+    toast.innerHTML = "<span>Icon</span> &nbsp;copied to clipboard &nbsp;🎉";
+    document.body.appendChild(toast);
 }
 
-function handleClassesChanged(){
-    if(!iconDetailSidebarOpen && sideNav.classList.contains('mat-drawer-opened')){
-        if(!downloadButton){
-            downloadButton = document.querySelector(".side-nav-links__download-buttons > a:first-child");
-            createCopyButton();
+function setupSidebarListener(sidebar){
+    const observer = new MutationObserver(function(mutations) {
+        const classChanged = mutations.find(({attributeName}) => attributeName == "class");
+
+        if(classChanged){
+            if(sidebar.classList.contains('mat-drawer-opened')){
+                if(!copyButton)
+                    replaceDownloadIconWithCopy();
+            }
+            else
+                copyButton = null;
         }
-    }
+    });
+
+    observer.observe(sidebar, {attributes: true});
 }
 
-function createCopyButton(){
-    copyButton = document.createElement("button");
-    copyButton.style.width = "100%";
-    copyButton.innerHTML = /*html*/`
-        <span class="icon-asset material-icons"
-            style="font-size: 14px; margin-right: 0.3rem;"
-        >
-            content_copy
-        </span>
-        COPY
-    `;
-    copyButton.className = downloadButton.className;
-    downloadButton.parentElement.prepend(copyButton);
+function replaceDownloadIconWithCopy(){
+    downloadButton = document.querySelector(".side-nav-links__download-buttons a");
     downloadButton.style.display = "none";
+
+    copyButton = document.createElement("button");
+    copyButton.innerHTML = /*html*/`
+        <span class="material-icons" style="font-size: 14px; margin-right: 0.8rem">content_copy</span>
+        <span style="margin-right: 0.4rem">SVG</span>
+    `;
+    copyButton.style.width = "100%";
+    copyButton.style.marginRight = "0.75rem";
+    copyButton.className = downloadButton.className;
+
+    downloadButton.parentElement.prepend(copyButton);
 
     copyButton.addEventListener("click", copyIcon);
 }
 
-async function copyIcon() {
-    const value = downloadButton.getAttribute("href");
-    const icon = await downloadIcon(value);
-    await navigator.clipboard.writeText(icon);
-}
-
-async function downloadIcon(iconPath) {
-    const res = await fetch(iconPath);
+async function copyIcon(){
+    const res = await fetch(downloadButton.getAttribute("href"));
     const icon = await res.text();
-    return icon;
+    await navigator.clipboard.writeText(icon);
+    toast.classList.add("show");
+
+    toast.addEventListener('transitionend', () => {
+        setTimeout(() => {
+            toast.classList.remove("show");
+        }, 1000);
+    });
+
+    toast.querySelector("span").innerHTML = icon;
 }
