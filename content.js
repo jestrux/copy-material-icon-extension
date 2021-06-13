9bcb6ad3eb25c703d54d5cf77f95fb4f58f219b8
Walter <wakyj07@gmail.com>
2021-06-13 12:34:09 +0300

    Replace copy icon with svg

--- content.js
+++ content.js
@@ -41,7 +41,7 @@ function replaceDownloadIconWithCopy(){
 
     copyButton = document.createElement("button");
     copyButton.innerHTML = /*html*/`
-        <span class="material-icons" style="font-size: 14px; margin-right: 0.8rem">content_copy</span>
+        <svg fill="currentColor" style="margin-right: 0.8rem" height="14" width="14" viewBox="0 0 24 24"><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"/></svg>
         <span style="margin-right: 0.4rem">SVG</span>
     `;
     copyButton.style.width = "100%";
